f974fa47c3fd15988a4e825d3e45e5770c5054c4
Christian Dragnev <kristian9107@gmail.com>
2020-11-27 16:39:27 +0200

    Events Model Migration and Controller created with resource Categories Model Migration and Controller created with resource

---
+++ database/migrations/2020_11_27_131450_create_events_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateEventsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('events', function (Blueprint $table) {
+            $table->id();
+            $table->enum('status', ['hidden', 'active']);
+            $table->string('language', 2);
+            $table->date('start_on');
+            $table->date('end_on')->nullable();
+            $table->integer('category_id')->index()->unsigned();
+            $table->string('title');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('events');
+    }
+}
